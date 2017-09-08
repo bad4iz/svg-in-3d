23e4366610bef452e9d7eedc9b6ea1cdf374fcbb
bad4iz <bad4iz@gmail.com>
2017-09-08 21:06:49 +0400

    sin 2

--- index.js
+++ index.js
@@ -2,70 +2,80 @@ var camera, controls, scene, renderer, geomentry;
 
 function init() {
 
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color(0xcccccc);
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xcccccc);
 
-    renderer = new THREE.WebGLRenderer();
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
-    var container = document.getElementById('container');
-    container.appendChild(renderer.domElement);
+  var container = document.getElementById('container');
+  container.appendChild(renderer.domElement);
 
-    camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 1, 3000);
-    camera.position.z = 500;
+  camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 1, 3000);
+  camera.position.z = 500;
 
-    controls = new THREE.OrbitControls(camera, renderer.domElement);
+  controls = new THREE.OrbitControls(camera, renderer.domElement);
 
 
-    //  что есть на сцене
-    var texture = (new THREE.TextureLoader).load('particle.png');
-    var material = new THREE.PointCloudMaterial({
-        size: 10,
-        vertexColors: THREE.VertexColors,
-        map: texture
-    });
+  //  что есть на сцене
+  var texture = (new THREE.TextureLoader).load('particle.png');
+  var material = new THREE.PointCloudMaterial({
+    size: 10,
+    vertexColors: THREE.VertexColors,
+    map: texture
+  });
 
-    geomentry = new THREE.Geometry();
-    var x, y, z;
+  geomentry = new THREE.Geometry();
+  var x, y, z;
 
-    // Точки
-    for (var i = 0; i <= 100; i++) {
-        x = Math.sin(i/10)*100;
-        y = 0;
-        z = i*10;
+  // Точки
+  for (var i = 0; i <= 100; i++) {
+    x = Math.sin(i / 10) * 100;
+    y = 0;
+    z = i * 10;
 
-        geomentry.vertices.push(new THREE.Vector3(x, y, z));
-        geomentry.colors.push(new THREE.Color(Math.random(), Math.random(), Math.random()));
-    }
+    geomentry.vertices.push(new THREE.Vector3(x, y, z));
+    geomentry.colors.push(new THREE.Color(Math.random(), Math.random(), Math.random()));
+  }
 
-    var pointCloud = new THREE.PointCloud(geomentry, material);
-    scene.add(pointCloud);
-    // конец сцены
+  var pointCloud = new THREE.PointCloud(geomentry, material);
+  scene.add(pointCloud);
+  // конец сцены
 
-    window.addEventListener('resize', onWindowResize, false);
+  window.addEventListener('resize', onWindowResize, false);
 }
 
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth / window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth / window.innerHeight);
 }
 
 var i = 0;
 
 function animate() {
-    i++;
-    requestAnimationFrame(animate);
+  i++;
+  requestAnimationFrame(animate);
+
+  geomentry.vertices.forEach((particle, index) => {
+    let dX, dY, dZ;
+    dX = Math.sin(i/10 + index/2)/2;
+    dY = 0;
+    dY = 0;
+
+      particle.add(new THREE.Vector3(dX, dY, dZ))
+
+});
 
 
-    geomentry.verticesNeedUpdate = true;
+  geomentry.verticesNeedUpdate = true;
 
-    render();
+  render();
 }
 
 function render() {
-    renderer.render(scene, camera);
+  renderer.render(scene, camera);
 }
 
 init();
